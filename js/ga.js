536c966f518c3bb92c6a5c74e537b305dbe36f47
Anna-Sara Isakson <annasaraisakson@hotmail.com>
2024-01-30 10:39:45 +0100

    gtag test

---
+++ js/ga.js
@@ -0,0 +1,7 @@
+document.getElementById("projects").addEventListener("click", function () {
+  gtag("event", "action", {
+    event_category: "click",
+    event_label: "projects",
+    value: value,
+  });
+});
