8e7e5b2ae21120fbf6c5062210dee0a0c397635f
Anna-Sara Isakson <annasaraisakson@hotmail.com>
2024-01-30 11:17:14 +0100

    gtag test4

--- js/ga.js
+++ js/ga.js
@@ -1,7 +1,7 @@
-document.querySelector("projects").addEventListener("click", function () {
-  gtag("event", "action", {
-    event_category: "click",
-    event_label: "projects",
-    value: value,
+document.querySelector("#projects").addEventListener("click", function () {
+  gtag("event", "click", {
+    event_category: "button_click",
+    event_label: "projects_button",
+    value: 1,
   });
 });
