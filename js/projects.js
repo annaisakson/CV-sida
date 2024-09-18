3e9144f4b59d7a4fcf48c8cae5ffb61271a043e7
Anna-Sara Isakson <annasaraisakson@hotmail.com>
2024-09-18 19:48:32 +0200

    added some more projects and fixed some minor css

--- js/projects.js
+++ js/projects.js
@@ -1,56 +1,66 @@
-// MODAL 
-// Original skrivet av "https://codepen.io/kevinpowell/pen/gxZMvR" 
-// Modifierad av mig 
-//***********************/
-const modalToggle = document.querySelector(".project-card1"),
-      modal = document.querySelector(".modal"),
-      closeButton = document.querySelector(".close");
+// Select all project cards and the modal element
+const projectCards = document.querySelectorAll(".project-cards");
+const modal = document.querySelector(".modal");
+const closeButton = modal.querySelector(".close");
 
-// If there is a modal button, open the modal when it's clicked
-// and close it if someone clicks the close button
-if (modalToggle) {
-    
-  modalToggle.addEventListener("click", () => openModal());
-  closeButton.addEventListener("click", () => closeModal());
-  
-  function openModal() {
-    fadeIn(modal);
-  };
-  
-  function closeModal() {
-    fadeOut(modal);
-  };
-  
-  function fadeOut(element) {
-    var opacity = 1;
-    function decrease() {
-      opacity -= 0.05;
-      if (opacity <=0) {
-        //complete
-        element.style.opacity = 0;
-        element.classList.remove('is-open');
-        return true;
-      }
-      element.style.opacity = opacity;
-      requestAnimationFrame(decrease);
-    }
-    decrease();
-  };
-  
-  function fadeIn(element) {
-    var opacity = 0;
-    element.classList.add('is-open');
-    function increase() {
-      opacity += 0.05;
-      if (opacity >=1) {
-        //complete
-        element.style.opacity = 1;
-        return true;
-      }
-      element.style.opacity = opacity;
-      requestAnimationFrame(increase);
-    }
-    increase();
-  };
+// Function to open the modal
+function openModal(modalContent) {
+  modal.innerHTML = modalContent;
+  fadeIn(modal);
 
+  // Add event listener to the new close button
+  modal.querySelector(".close").addEventListener("click", closeModal);
+}
+
+// Function to close the modal
+function closeModal() {
+  fadeOut(modal);
+}
+
+// Add click event to all project cards
+projectCards.forEach((card, index) => {
+  card.addEventListener("click", () => {
+    // Fetch the content of the corresponding inner modal
+    const innerModalId = `inner-modal-${index + 1}`;
+    const innerModal = document.getElementById(innerModalId);
+
+    if (innerModal) {
+      // If the inner modal exists, use its content
+      openModal(innerModal.innerHTML);
+    } else {
+      console.error(`Modal content not found for id: ${innerModalId}`);
+    }
+  });
+});
+
+function fadeOut(element) {
+  var opacity = 1;
+  function decrease() {
+    opacity -= 0.05;
+    if (opacity <= 0) {
+      //complete
+      element.style.opacity = 0;
+      element.classList.remove("is-open");
+      return true;
+    }
+    element.style.opacity = opacity;
+    requestAnimationFrame(decrease);
+  }
+  decrease();
+}
+
+function fadeIn(element) {
+  var opacity = 0;
+  element.classList.add("is-open");
+  function increase() {
+    opacity += 0.05;
+    if (opacity >= 1) {
+      //complete
+      element.style.opacity = 1;
+      return true;
+    }
+    element.style.opacity = opacity;
+    requestAnimationFrame(increase);
+  }
+  increase();
 }
